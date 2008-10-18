a2c29003dc7e46d235eb4a4b7e7cccf9e2fed7b9
todesking <todesking@d64612fe-358e-fc41-994c-71b123da8b90>
2008-10-18 14:59:38 +0000

    define AccountHistory.amount_at method
    git-svn-id: svn+ssh://todesking.com/home/todesking/svnrepos/main@679 d64612fe-358e-fc41-994c-71b123da8b90

--- spec/model_account_history_spec.rb
+++ spec/model_account_history_spec.rb
@@ -1,6 +1,6 @@
 require 'spec/model_spec_helper.rb'
 
-describe AccountHistory,'with empty' do
+describe AccountHistory,'with empty, and no transactions' do
   before(:all) do
     ModelSpecHelper.setup_database
   end
@@ -14,6 +14,34 @@ describe AccountHistory,'with empty' do
   end
 end
 
-describe AccountHistory,'with some histories' do
+describe AccountHistory,'with some histories, and no transactions' do
+  before(:all) do
+    ModelSpecHelper.setup_database
+  end
+  before(:each) do
+    AccountHistory.delete_all
+    Transaction.delete_all
+    [
+      { :date => '2008-10-01', :name => 'bank', :amount => 1000},
+      { :date => '2008-10-02', :name => 'bank', :amount => 2000},
+      { :date => '2008-10-03', :name => 'bank', :amount => 3000},
+      { :date => '2008-10-04', :name => 'bank', :amount => 1500}
+    ].each{|t|
+      AccountHistory.new(t).save
+    }
+  end
+  it 'should 0 yen in bank before history begins' do
+    AccountHistory.amount_at(:bank,'2008-09-30').should be(0)
+    AccountHistory.amount_at(:bank,'2007-10-03').should be(0)
+  end
+end
 
+describe AccountHistory,'with no histories, and some transactions' do
+  before(:all) do
+    ModelSpecHelper.setup_database
+  end
+  before(:each) do
+    AccountHistory.delete_all
+    Transaction.delete_all
+  end
 end
--- src/model/account_history.rb
+++ src/model/account_history.rb
@@ -3,6 +3,9 @@ require 'active_record'
 
 class AccountHistory < ActiveRecord::Base
   def self.current_amount(stash_name)
+    amount_at(stash_name,Date.new)
+  end
+  def self.amount_at(stash_name,date)
     0
   end
 end
