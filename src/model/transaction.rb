af20906aa5ed43d95af2d3c55b3d3f9c0f248f52
todesking <todesking@d64612fe-358e-fc41-994c-71b123da8b90>
2008-10-20 14:23:02 +0000

    Transaction.balance_between error message
    git-svn-id: svn+ssh://todesking.com/home/todesking/svnrepos/main@709 d64612fe-358e-fc41-994c-71b123da8b90

--- src/model/transaction.rb
+++ src/model/transaction.rb
@@ -4,7 +4,7 @@ require 'active_record'
 class Transaction < ActiveRecord::Base
   # note: from,to is Date, inclusive
   def self.balance_between(stash_name,from,to)
-    raise ArgumentError unless from <= to
+    raise ArgumentError.new('from > to') unless from <= to
     expenses=Transaction.sum('amount',:conditions=>['src = ? and ? <= date and date <= ?',stash_name,from,to])
     income=Transaction.sum('amount',:conditions=>['dest = ? and ? <= date and date <= ?',stash_name,from,to])
     return income - expenses
