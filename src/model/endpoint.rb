0be38a0c662d3582e12ffd4117923b2429b7843f
todesking <todesking@d64612fe-358e-fc41-994c-71b123da8b90>
2008-10-20 14:23:09 +0000

    refactor: Endpoint.amount_at
    git-svn-id: svn+ssh://todesking.com/home/todesking/svnrepos/main@712 d64612fe-358e-fc41-994c-71b123da8b90

--- src/model/endpoint.rb
+++ src/model/endpoint.rb
@@ -1,15 +1,10 @@
 class Endpoint < ActiveRecord::Base
   def amount_at(at)
-    amount=0
     history=AccountHistory.newest_history(self.name,at)
-    amount=history.amount unless history.nil?
     if history.nil?
-      oldest_transaction=Transaction.find(:first,:conditions=>['(src = ? or dest = ?) and date <= ?',self.name,self.name,at],:order=>'date')
-      amount+=Transaction.balance_between(self.name,oldest_transaction.date,at) unless oldest_transaction.nil?
+      return 0 + Transaction.balance_between(self.name,nil,at)
     else
-      start_date=history.date
-      amount+=Transaction.balance_between(self.name,start_date,at)
+      return history.amount + Transaction.balance_between(self.name,history.date,at)
     end
-    amount
   end
 end
