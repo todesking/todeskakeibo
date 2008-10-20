7f93db43aee5ccc4f046d1ef0b4abb61722382b2
todesking <todesking@d64612fe-358e-fc41-994c-71b123da8b90>
2008-10-20 14:23:55 +0000

    implement error check to CommandContext#date
    git-svn-id: svn+ssh://todesking.com/home/todesking/svnrepos/main@731 d64612fe-358e-fc41-994c-71b123da8b90

--- spec/commmand_context_spec.rb
+++ spec/commmand_context_spec.rb
@@ -10,4 +10,13 @@ describe CommandContext do
   it 'should can specify base date' do
     @context.base_date=Date.new #only check no-error
   end
+  it 'should create date from mmdd based on base_date' do
+    pending
+    @context.base_date=Date.new(2008,10,10)
+    @context.date('1001').should be == Date.new(2008,10,1)
+  end
+  it 'should error when date called with wrong format passed' do
+    lambda { @context.date('totally-wrong') }.should raise_error(ArgumentError)
+    lambda { @context.date('11299000900') }.should raise_error(ArgumentError)
+  end
 end
--- src/command_context.rb
+++ src/command_context.rb
@@ -3,6 +3,8 @@ class CommandContext
     case str
     when /(\d{4})(\d{2})(\d{2})/
       Date.new($1.to_i,$2.to_i,$3.to_i)
+    else
+      raise ArgumentError.new("unknown format date string: #{str}")
     end
   end
   attr_accessor :base_date
