bfdc274aedbbd14981b7c0bae1bbcfba024e99b4
todesking <todesking@d64612fe-358e-fc41-994c-71b123da8b90>
2008-10-22 14:24:20 +0000

    add console
    git-svn-id: svn+ssh://todesking.com/home/todesking/svnrepos/main@765 d64612fe-358e-fc41-994c-71b123da8b90

---
+++ src/console.rb
@@ -0,0 +1,33 @@
+puts 'loading...'
+require 'rubygems'
+require 'activerecord'
+
+require File.dirname(__FILE__)+'/'+'controller.rb'
+require File.dirname(__FILE__)+'/'+'model/helper.rb'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :dbfile => ':memory:'
+)
+
+controller=Controller.new
+
+continue=true
+
+controller.define_command('exit',[]) { continue=false }
+controller.define_command('initialize_database',[]) {
+  ModelHelper.create_tables
+  'done.'
+}
+
+while(continue)
+  print 'kakeibo> '
+  cmd=gets
+  begin
+    resp=controller.execute(cmd)
+    puts ' => '+resp.to_s
+  rescue => e
+    puts 'ERRORR: '+e
+  end
+end
+puts 'bye.'
