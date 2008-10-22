132b5d1fc431d7bef7fd3cae432d5a4d5b4fe040
todesking <todesking@d64612fe-358e-fc41-994c-71b123da8b90>
2008-10-22 14:25:00 +0000

    update console/controller
    git-svn-id: svn+ssh://todesking.com/home/todesking/svnrepos/main@779 d64612fe-358e-fc41-994c-71b123da8b90

--- src/console.rb
+++ src/console.rb
@@ -7,7 +7,7 @@ require File.dirname(__FILE__)+'/'+'model/helper.rb'
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :dbfile => ':memory:'
+  :dbfile => './kakeibo.sqlite3'
 )
 
 controller=Controller.new
@@ -25,8 +25,11 @@ while(continue)
   cmd=gets
   begin
     resp=controller.execute(cmd)
-    puts ' => '+resp.to_s
+    resp_str=(resp.respond_to? :to_str)? resp.to_str : resp.to_s
+    puts resp_str.split("\n").map{|l|'  '+l}
   rescue => e
-    puts 'ERRORR: '+e
+    puts 'ERRORR: '
+    str=(e.respond_to? :to_str)? e.to_str : e.to_s
+    puts str.split("\n").map{|l|'  '+l}
   end
 end
--- src/controller.rb
+++ src/controller.rb
@@ -8,6 +8,8 @@ class Controller
   def initialize
     @parser=CommandParser.new
     @date_parser=RelativeDateParser.new
+    parser=@parser
+    date_parser=@date_parser
 
     # define argument types
     @parser.type_parser.define_mapping(Endpoint) {|ep_name|
@@ -22,22 +24,69 @@ class Controller
     define_command('transaction',[[:date,Date], [:src,Endpoint], [:dest,Endpoint], [:amount,Numeric]]) do
       Transaction.new(:date=>@date, :src=>@src, :dest=>@dest, :amount=>@amount).save
     end
+    define_alias(['t'],'transaction')
+
     define_command('account',[[:date,Date], [:endpoint,Endpoint], [:amount,Numeric]]) do
       AccountHistory.new(:date=>@date, :endpoint=>@endpoint, :amount=>@amount).save
     end
-    date_parser=@date_parser
+    define_alias(['a'],'account')
+
     define_command('base_date',[[:date,Date]]) do
       date_parser.base_date=@date
     end
+    define_alias('bd','base_date')
+
     define_command('endpoint',[[:ep_name,String],[:parent,Endpoint,{:default=>nil}]]) do
       Endpoint.new(:name=>@ep_name,:parent=>@parent).save
     end
+    define_alias('ep','endpoint')
+    
     define_command('endpoint_alias',[[:alias_name,String],[:alias_for,Endpoint]]) do
       EndpointAlias.new(:name=>@alias_name,:endpoint=>@alias_for).save
     end
+    define_alias('epa','endpoint_alias')
+
+    # define commands(untestable, most of is 'show' command)
+    define_command('help',[[:sub_command,String,{:default=>nil}]]) do
+      default_str= <<EOS
+help : show this message
+help commands : show commands with aliases
+EOS
+      case @sub_command
+      when 'commands'
+        lines=[]
+        parser.non_alias_commands.each{|k,v|
+          lines << "#{v.to_str}"
+          lines << "  aliases: #{parser.aliases_for(v).map{|k,v|k}.join(' ')}" unless parser.aliases_for(v).empty?
+        }
+        lines.join("\n")
+      when nil
+        default_str
+      else
+        default_str
+      end
+    end
+    define_alias(['h','?','he'],'help')
+
+    define_command('endpoints',[]) do
+      def epp(res,level,ep)
+        res << '  '*level + ep.name
+        ep.children.each{|c| epp(res,level+1,c)}
+      end
+      lines=['Endpoints:']
+      ep_root=Endpoint.find(:all,:conditions=>{:parent=>nil})
+      ep_root.each{|e|
+        epp(lines,1,e)
+      }
+      lines.join("\n")
+    end
+    define_alias('eps','endpoints')
   end
   def define_command(name,defs,&block)
     @parser.define_command(name,defs,&block)
+  end
+  def define_alias(aliases,command)
+    @parser.define_alias(aliases,command)
   end
   def execute command
     case command
